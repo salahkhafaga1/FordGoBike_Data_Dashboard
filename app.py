08b559b03d93b4d8f6b5dc2ab94a2d5fcfc8f719
salahkhafaga1 <salahkhafaga1234@gmail.com>
2026-03-09 01:32:51 +0200

    Prepare app for deployment with gunicorn

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import dash_bootstrap_components as dbc
 from pages import home, dashboard, insights, about
 
 # Initialize the app with a Bootstrap theme
-# LUX بيدي شكل احترافي جداً وخطوط عريضة شيك
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX], suppress_callback_exceptions=True)
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LITERA], suppress_callback_exceptions=True)
+
+server = app.server
+
 app.title = "Ford GoBike App"
 
 # Navbar Setup
